dfcdce6120f47859ad12d4dccaf8ad077112e4c9
Levi <levi@novaling.co.uk>
2017-05-20 04:53:42 -0700

    Revert "update gitignore so I can work on laptop"
    This reverts commit 1eeba370b0adc7c30f3379a366bc041840d9d22a.

--- Source/Carnivale/CarnivaleMod.cs
+++ Source/Carnivale/CarnivaleMod.cs
@@ -1,11 +1,15 @@
-﻿using HugsLib;
+﻿using Harmony;
+using HugsLib;
 using Verse;
+using System.Reflection;
 
 namespace Carnivale
 {
     [StaticConstructorOnStartup]
     public class CarnivaleMod : ModBase
     {
+        public static readonly string id = "com.github.xnope.carnivale";
+
         public override string ModIdentifier { get { return "Carnivale"; } }
 
         public static readonly bool debugLog = true;
